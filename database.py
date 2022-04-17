9a44d3b74cb2810fef9f80de6a5d8d5004dc963d
Daniil <zzzgahihzzz@gmail.com>
2022-04-18 00:03:23 +0300

    save

--- database.py
+++ database.py
@@ -24,28 +24,29 @@ def set_user(user_id, username, first_name, last_name):
                     f"VALUES ('{user_id}', '{username}', '{first_name}', '{last_name}');")
         conn.commit()
 
+while True:
+    def set_primMRC(download_link):
+        cur.execute(f"SELECT download_link FROM prim_MRC WHERE download_link = '{download_link}'")
+        data = cur.fetchone()
+        if data is None:
+            cur.execute(f"INSERT INTO prim_MRC (download_link) VALUES ('{download_link}')")
+            conn.commit()
+            cur.execute(f"SELECT user_id FROM users WHERE subscription = TRUE")
+            idmail = cur.fetchone()
+            MRC_bot.bot.send_message(idmail, 'Вышло новое обновление применяемого МРЦ')
 
-def set_primMRC(download_link):
-    cur.execute(f"SELECT download_link FROM prim_MRC WHERE download_link = '{download_link}'")
-    data = cur.fetchone()
-    if data is None:
-        cur.execute(f"INSERT INTO prim_MRC (download_link) VALUES ('{download_link}')")
-        conn.commit()
-        cur.execute(f"SELECT user_id FROM users WHERE subscription = TRUE")
-        idmail = cur.fetchone()
-        MRC_bot.bot.send_message(idmail, 'Вышло новое обновление применяемого МРЦ')
 
 
+    def set_zayavMRC(download_link):
+        cur.execute(f"SELECT download_link FROM zayav_MRC WHERE download_link = '{download_link}'")
+        data = cur.fetchone()
+        if data is None:
+            cur.execute(f"INSERT INTO zayav_MRC (download_link) VALUES ('{download_link}')")
+            conn.commit()
+            cur.execute(f"SELECT user_id FROM users WHERE subscription = TRUE")
+            idmail = cur.fetchone()
+            MRC_bot.bot.send_message(idmail, 'Вышло новое обновление применяемого МРЦ')
 
-def set_zayavMRC(download_link):
-    cur.execute(f"SELECT download_link FROM zayav_MRC WHERE download_link = '{download_link}'")
-    data = cur.fetchone()
-    if data is None:
-        cur.execute(f"INSERT INTO zayav_MRC (download_link) VALUES ('{download_link}')")
-        conn.commit()
-        cur.execute(f"SELECT user_id FROM users WHERE subscription = TRUE")
-        idmail = cur.fetchone()
-        MRC_bot.bot.send_message(idmail, 'Вышло новое обновление применяемого МРЦ')
 
 def sub(user_id):
     cur.execute(f"UPDATE users SET subscription = TRUE WHERE user_id = '{user_id}';")
