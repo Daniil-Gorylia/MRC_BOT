11aaddb942dafcd27ad777dc19d0d86569bedb66
Daniil <zzzgahihzzz@gmail.com>
2022-04-18 14:25:28 +0300

    save

--- mailing.py
+++ mailing.py
@@ -3,9 +3,11 @@ import database
 
 
 def mailing_all():
-    resp = f"https://nalog.gov.by/{content.MRC_primen()[0]}"
+    a = content.MRC_primen()[0]
+    b = content.MRC_zayav()[0]
+    resp = f"https://nalog.gov.by/{a}"
     database.set_primMRC(resp)
-    resp2 = f"https://nalog.gov.by/{content.MRC_zayav()[0]}"
+    resp2 = f"https://nalog.gov.by/{b}"
     database.set_zayavMRC(resp2)
 
 
