9c9309be28fffeaf6f85bee0c36c99b4dc6e837a
Daniil <zzzgahihzzz@gmail.com>
2022-04-18 16:58:29 +0300

    first commit

--- mailing.py
+++
@@ -1,9 +0,0 @@
-import content
-import database
-import MRC_bot
-
-def mailing_all():
-    MRC_bot.bot.send_message(database.sub_check()[0], "Вышло новое обновление заявленных мрц, проверьте ссылку")
-
-
-mailing_all()
